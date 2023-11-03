a46f85ab69ed3afa5af88ebf37e1df77d87961a2
BusySnow <thanglb.bi11-243@st.usth.edu.vn>
2023-11-03 16:12:09 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+  const handleInput = (value) => {
+    setInput(input + value);
+  };
+
+  const handleDel = () => {
+    setInput(input.slice(0, -1));
+  };
+
+  const handleClear = () => {
+    setInput("");
+  };
+
+  const calculateResult = () => {
+    try {
+      if (/[\d-+*/.]--|[\d-+*/.]\+\+/.test(input) || /[-+*/.]$/.test(input)) {
+        setInput("Syntax Error");
+      } else {
+        setInput(eval(input));
+      }
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="cal-container">
+        <input type="text" value={input} className="display" readOnly />
+        <div className="button-container">
+          <div className="row">
+            <button onClick={() => handleInput("7")}>7</button>
+            <button onClick={() => handleInput("8")}>8</button>
+            <button onClick={() => handleInput("9")}>9</button>
+            <button onClick={handleDel} className="clear">
+              DEL
+            </button>
+            <button onClick={handleClear} className="clear">
+              AC
+            </button>
+          </div>
+          <div className="row">
+            <button onClick={() => handleInput("4")}>4</button>
+            <button onClick={() => handleInput("5")}>5</button>
+            <button onClick={() => handleInput("6")}>6</button>
+            <button onClick={() => handleInput("*")} className="operation">
+              *
+            </button>
+            <button onClick={() => handleInput("/")} className="operation">
+              /
+            </button>
+          </div>
+          <div className="row">
+            <button onClick={() => handleInput("1")}>1</button>
+            <button onClick={() => handleInput("2")}>2</button>
+            <button onClick={() => handleInput("3")}>3</button>
+            <button onClick={() => handleInput("+")} className="operation">
+              +
+            </button>
+            <button onClick={() => handleInput("-")} className="operation">
+              -
+            </button>
+          </div>
+          <div className="row">
+            <button onClick={() => handleInput("0")}>0</button>
+            <button onClick={() => handleInput(".")}>.</button>
+            <button onClick={calculateResult} className="equal">
+              =
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
